ba7fa0ded45d5bbdcda4c03a6f0557e37963b639
gpartch <143952440+gpartch@users.noreply.github.com>
2023-11-16 15:42:25 -0700

    Added GabriellePartch.js to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,6 +26,9 @@ export default function Home() {
             <h3>Example About Me Page &rarr;</h3>
             <p>Click here to navigate to an example page</p>
           </a>
+          <a href='/GabriellePartch ' className='card'>
+            <h3>Gabrielle Partch &rarr;</h3>
+          </a>
 
           <a
             href='https://github.com/vedant29/Project-CSCI-1000'
