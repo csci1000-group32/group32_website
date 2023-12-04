7232b01f46bf5bb36a93e3cd4bfcab82a9cc3a04
Alex C <alca1296@colorado.edu>
2023-12-03 23:35:43 -0700

    made various changes to my page to make it look better and added info

--- src/pages/AlexCarmichael.js
+++ src/pages/AlexCarmichael.js
@@ -9,15 +9,17 @@ export default function examplePage() {
             <h1 className='title'>Alex Carmichael</h1>
           </div>
 
-
           <p className='description'>
-            Hi, I'm a freshman at CU Boulder studying Computer Science'
+            Hi, I'm a freshman at CU Boulder studying Computer Science
           </p>
           <img
             className='pic'
             src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.colorado.edu%2Ftoday%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Flarge%2Fpublic%2Fcallout%2Fcu_boulder_earlyam_aerial_ga.jpg%3Fitok%3DmtrOfHFM&f=1&nofb=1&ipt=7529739b461fd35e8ba6645fde2a2928b0073904c2d0f53eeb6080080e80bf7d&ipo=images'
-            alt='tom holland'
+            alt='CU Boulder Campus'
           />
+          <p className='description'>
+            I'm very excited to study CS here and can't wait to get real world experiences with programming through internships or jobs.
+          </p>
           <a className='button' href='/'>
             <p> Back to Home Page</p>
           </a>
@@ -36,12 +38,13 @@ export default function examplePage() {
           }
 
           main {
-            padding: 5rem 0;
+            padding: 2rem 5rem;
             flex: 1;
             display: flex;
             flex-direction: column;
             justify-content: center;
             align-items: center;
+            background: #FFFDD0;
           }
 
           .title {
@@ -62,7 +65,6 @@ export default function examplePage() {
 
           .pic {
             height: 450px;
-            width: 450px;
           }
 
           .button {
@@ -75,6 +77,7 @@ export default function examplePage() {
             border: 1px solid #eaeaea;
             border-radius: 10px;
             transition: color 0.15s ease, border-color 0.15s ease;
+            background: #FFFFFF;
           }
 
           .button:hover,
@@ -104,8 +107,7 @@ export default function examplePage() {
           body {
             padding: 0;
             margin: 0;
-            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            font-family: Noto Sans, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
               sans-serif;
           }
 
